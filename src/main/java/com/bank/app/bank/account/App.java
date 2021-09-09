20ba9bb9256685d26a903a42634a8c63d087b8a5
Ameni HADRICH <ahadrich@afdtech.com>
2021-09-09 18:42:10 +0200

    US3: see the history of operations

--- src/main/java/com/bank/app/bank/account/App.java
+++ src/main/java/com/bank/app/bank/account/App.java
@@ -1,7 +1,9 @@
 package com.bank.app.bank.account;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import com.bank.app.bank.account.entites.Account;
 import com.bank.app.bank.account.entites.Balance;
@@ -17,10 +19,11 @@ public class App {
 	
 	private static OperationServiceImpl opService = new OperationServiceImpl();
 	
+	private static List<Operation> historyOperations = new ArrayList<Operation>();
 	
     public static void main( String[] args )
     {
-        System.out.println( "Welcome To Bank Account Application !" );
+        System.out.println( "Welcome To Bank Account Application ! \n" );
         // create Account 1 : 
         String numAccount1 = "FR123456";
         double amountAccount1 = 0.0;
@@ -28,19 +31,25 @@ public class App {
         Account account1 = new Account(balanceAccount1,amountAccount1);
         
         try {
-			Operation depotOperation = opService.depositeAccount(account1, 100.0);
-			Operation creditOperation = opService.withdrawalAccount(account1, 50.0);
+        	Operation operation = opService.depositeAccount(account1, 100.0);
+			historyOperations.add(operation);
+			operation = opService.withdrawalAccount(account1, 50.0);
+			historyOperations.add(operation);
 			
+			List<Operation> operations = opService.historyOperation(account1,historyOperations);
 			System.out.println("Account detail :");
 			System.out.println(" Number : " + numAccount1);
 			System.out.println(" Amount : " + account1.getAmount());
 			System.out.println(" total debits : " + account1.getBalance().getTotalDebits());
 			System.out.println(" total credits : " + account1.getBalance().getTotalCredits());
-			
-			System.out.println(" Date : " + formatDate(depotOperation.getDate()) + " | Type : " + depotOperation.getOperationType()
-			+ " | Operation Amount : " + depotOperation.getAmount());
-			System.out.println(" Date : " + formatDate(creditOperation.getDate()) + " | Type : " + creditOperation.getOperationType()
-			+ " | Operation Amount : " + creditOperation.getAmount());
+
+			System.out.println("Operation details :");
+			for (Operation op : operations) {
+			    
+				System.out.println(" Date : " + formatDate(op.getDate()) + " | Type : " + op.getOperationType()
+						+ " | Operation Amount : " + op.getAmount());
+				
+			}
 						
 		} catch (Exception e) {
 			e.printStackTrace();
--- src/main/java/com/bank/app/bank/account/services/IOperationService.java
+++ src/main/java/com/bank/app/bank/account/services/IOperationService.java
@@ -1,5 +1,9 @@
 package com.bank.app.bank.account.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bank.app.bank.account.dao.OperationDAO;
 import com.bank.app.bank.account.entites.Account;
 import com.bank.app.bank.account.entites.Operation;
 
@@ -27,6 +31,37 @@ public interface IOperationService {
 	 * @throws Exception ex
 	 */
 	public abstract Operation withdrawalAccount(Account account, double amount) throws Exception;
+	
+	/**
+	 * Operations history for the given account
+	 * @param account
+	 * @return List of Operations
+	 */
+	default List<Operation> historyOperation(Account account) {
+		// retrieve all operations for the user account param
+		OperationDAO opDAO = new OperationDAO();
+		String numeroAccount = account.getBalance().getAccountNumber();
+		List<Operation> operations = opDAO.findByAccount(numeroAccount);
+		return operations;
+	}
+	
+	/**
+	 * Operations history for the given account and from the list of operations
+	 * @param account
+	 * @param operations
+	 * @return list of operations
+	 */
+	default List<Operation> historyOperation(Account account, List<Operation> operations) {
+		// retrieve all operations for the user account param from list of operations
+		String numeroAccount = account.getBalance().getAccountNumber();
+		List<Operation> history = new ArrayList<Operation>();
+		for (Operation operation : operations) {
+			if(operation.getAccount().getBalance().getAccountNumber().equals(numeroAccount)) {
+				history.add(operation);
+			}
+		}
+		return history;
+	}
 	
 	
 }
